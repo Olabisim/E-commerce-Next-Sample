c9c60c6ee3440e5bb9628b975bd1cd5b599552ac
Olabisim <olabisiajoseh@gmail.com>
2024-09-13 11:32:31 +0100

    changed top pictures

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -1,8 +1,8 @@
 
 export const products = [
-    { id: 1, name: 'Basic Tee', price: 35, image: '../../top.JPG', description: 'A comfortable and versatile tee for everyday wear.', tag: 'shirts' },
-    { id: 2, name: 'Fancy Pants', price: 60, image: '../../top.JPG', description: 'Elevate your style with these trendy pants.', tag: 'shirts' },
-    { id: 3, name: 'Cool Hoodie', price: 75, image: '../../top.JPG', description: 'Stay warm and look cool with this awesome hoodie.', tag: 'shirts' },
+    { id: 1, name: 'Basic Tee', price: 35, image: 'https://img.freepik.com/free-photo/high-angle-man-standing_23-2148467256.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'A comfortable and versatile tee for everyday wear.', tag: 'shirts' },
+    { id: 2, name: 'Fancy Pants', price: 60, image: 'https://img.freepik.com/free-photo/high-angle-man-standing_23-2148467256.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'Elevate your style with these trendy pants.', tag: 'shirts' },
+    { id: 3, name: 'Cool Hoodie', price: 75, image: 'https://img.freepik.com/free-photo/high-angle-man-standing_23-2148467256.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'Stay warm and look cool with this awesome hoodie.', tag: 'shirts' },
     { id: 4, name: 'Basic Tee', price: 35, image: 'https://img.freepik.com/free-photo/spring-wardrobe-switch-still-life_23-2150176689.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'A comfortable and versatile tee for everyday wear.', tag: 'shoes' },
     { id: 5, name: 'Fancy Pants', price: 60, image: 'https://img.freepik.com/free-photo/spring-wardrobe-switch-still-life_23-2150176689.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'Elevate your style with these trendy pants.', tag: 'shoes' },
     { id: 6, name: 'Cool Hoodie', price: 75, image: 'https://img.freepik.com/free-photo/spring-wardrobe-switch-still-life_23-2150176689.jpg?ga=GA1.1.340841201.1717685554&semt=ais_hybrid', description: 'Stay warm and look cool with this awesome hoodie.', tag: 'shoes' },
